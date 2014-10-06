915e2d514de15c9cbf8bc9e7dad99d425b1be93e
Nate Woods <woods.nb@gmail.com>
2014-10-06 10:53:19 +0200

    Put plotting scripts into python path.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 # Get directory of script (rather than directory the script is run from)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export zza="${DIR}"
-export PYTHONPATH="${PYTHONPATH}":"${zza}"/analyzers:"${zza}"/ZZUtils:"${zza}"/ZZMetadata
+export PYTHONPATH="${PYTHONPATH}":"${zza}"/analyzers:"${zza}"/ZZUtils:"${zza}"/ZZMetadata:"${zza}"/plotting
 
 echo "ZZAnalyzer setup complete"
