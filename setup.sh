cf0276449faaa7735ad1fdde64a35657197bb25d
Nate Woods <woods.nb@gmail.com>
2015-03-23 11:36:01 +0100

    Include more utility areas in python path.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 # Get directory of script (rather than directory the script is run from)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export zza="${DIR}"
-export PYTHONPATH="${PYTHONPATH}":"${zza}"/analyzers:"${zza}"/ZZUtils:"${zza}"/ZZMetadata:"${zza}"/plotting
+export PYTHONPATH="${PYTHONPATH}":"${zza}"/analyzers:"${zza}"/ZZUtils:"${zza}"/ZZUtils/cutTemplates:"${zza}"/ZZUtils/resultTemplates:"${zza}"/ZZUtils/rowCleaners:"${zza}"/ZZMetadata:"${zza}"/plotting
 
 echo "ZZAnalyzer setup complete"
