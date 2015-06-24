9a43d19cb6e09a2fac6da9a1885a2b113f63cafc
Nate Woods <woods.nb@gmail.com>
2015-06-24 12:24:15 +0200

    Update 8TeV row cleaner to new version of cleaner.

--- ZZUtils/rowCleaners/HZZ4l2012Cleaner.py
+++ ZZUtils/rowCleaners/HZZ4l2012Cleaner.py
@@ -11,54 +11,47 @@ from ZZHelpers import * # evVar, objVar, nObjVar, Z_MASS
 
 
 class HZZ4l2012Cleaner(ZZRowCleanerBase):
-    def __init__(self, channel, cutter):
-        super(HZZ4l2012Cleaner, self).__init__(channel, cutter)
+    def __init__(self, cutter, initChannel='eeee'):
+        super(HZZ4l2012Cleaner, self).__init__(cutter, initChannel)
         self.cleanAtEnd = False # do cleaning first
-        self.prevDZ = 999.
-        self.prevPtSum = -999.
 
 
-    def isNewBest(self, row, newEvent):
+    def betterRow(self, a, b):
         '''
-        Returns True if this row is better than previous rows from the same
-        event it has just seen. The correct row is the one with Z1 closest
-        to on-shell, with the highest scalar Pt sum of the remaining leptons
-        used as a tiebreaker. Only Zs whose leptons pass ID are considered
-        (if no rows from an event pass ID, the first is considered best). 
+        Returns the better row. 
+        The better row is the one with Z1 closer to nominal Z mass, with
+        the scalar sum of the Z2 leptons' pTs used as a tiebreaker.
+        When in doubt, takes b.
         '''
-        # To store appropriate info, we always need to do the ID+iso check
-
-        # make a copy in case we have to reorder
-        objects = self.objectTemplate
-        
-        if self.needReorder:
-            objects = self.cuts.orderLeptons(row, self.channel, self.objectTemplate)
-
-        allGood = True
-        for lepts in [[objects[0],objects[1]],[objects[2],objects[3]]]:
-            if not self.cuts.doCut(row, "GoodZ", *lepts):
-                allGood = False
-                break
-            if not self.cuts.doCut(row, "ZIso", *lepts):
-                allGood = False
-                break
-
-        if allGood:
-            dZ = zCompatibility(row, objects[0], objects[1])
-            ptSum = objVar(row, 'Pt', objects[2]) + objVar(row, 'Pt', objects[3])
-        else:
-            dZ = 999
-            ptSum = -999
-
-        isBest = newEvent or (dZ < self.prevDZ or (dZ == self.prevDZ and ptSum > self.prevPtSum))
-
-        if isBest:
-            self.prevDZ = dZ
-            self.prevPtSum = ptSum
-        
-        return isBest
-       
- 
+        if a.dZ < b.dZ or (a.dZ == b.dZ and a.ptSum > b.ptSum):
+            return a
+        return b
 
 
+    class RowInfo(ZZRowCleanerBase.RowInfo):
+        def __init__(self, row, channel, idx, objects, cuts):
+            super(self.__class__, self).__init__(row, channel, idx, objects, cuts)
+            
+        def storeVars(self, row, objects, cuts):
+            '''
+            Need Z1 distance from nominal mass and scalar sum of pt of Z2 
+            leptons. We only care about this row if the leptons all pass ID
+            and isolation, so the variables are set to ridiculous values if 
+            the event does not pass.
+            '''
+            allGood = True
+            for lepts in [[objects[0],objects[1]],[objects[2],objects[3]]]:
+                if not cuts.doCut(row, "GoodZ", *lepts):
+                    allGood = False
+                    break
+                if not cuts.doCut(row, "ZIso", *lepts):
+                    allGood = False
+                    break
+
+            if allGood:
+                self.dZ = zCompatibility(row, objects[0], objects[1], False)
+                self.ptSum = objVar(row, 'Pt', objects[2]) + objVar(row, 'Pt', objects[3])
+            else:
+                self.dZ = 999
+                self.ptSum = -999
 
