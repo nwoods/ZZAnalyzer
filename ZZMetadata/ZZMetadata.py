7a73f7bd3882a3a20d02ef2d6d2f8cb5ee41f0a2
Nate <nwoods@hep.wisc.edu>
2015-07-31 14:50:33 +0200

    Remove old stuff, add 50ns samples, update to rootpy style formatting options.

--- ZZMetadata/ZZMetadata.py
+++ ZZMetadata/ZZMetadata.py
@@ -1,6 +1,6 @@
 '''
 
-Metadata needed for plotting 4l Monte Carlo (and associated backgrounds)
+Metadata needed for plotting samples relevant to 4l
 
 Preferred sample name format is datasetFromDAS_campaign_PUbxScenario[_dataTier if important -- probably won't be]
 
@@ -17,181 +17,82 @@ Author: Nate Woods, U. Wisconsin
 
 '''
 
-from rootpy.ROOT import EColor
-
 sampleInfo = {}
 
-sampleInfo["DYJetsToLL_M-50_13TeV-madgraph-pythia8-tauola_v2_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 6025.2,
-    'n' : 43630561,
+sampleInfo['data'] = {
+    'isData' : True,
+    'color' : 'black',
+    'shortName' : 'data',
+    'prettyName' : 'data',
+}
+
+sampleInfo["DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
+    'xsec' : 6104, #5943.2?
+    'n' : 19925500,
     'isData' : False,
     'shortName' : 'DYJets',
     'prettyName' : 'DY+Jets',
     'isSignal' : False,
-    'color' : EColor.kAzure+7,
+    'color' : 'forestgreen',
 }
-sampleInfo["VBF_HToZZTo4L_M-125_13TeV-powheg-pythia6_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 3.72 * 0.000294,
-    'n' : 193738,
+
+sampleInfo["GluGluToZZTo2e2mu_BackgroundOnly_13TeV_MCFM"] = {
+    'xsec' : 0.00319364,
+    'n' : 649600,
     'isData' : False,
-    'shortName' : 'VBFHZZ4l',
-    'prettyName' : '\\text{VBF H}\\!\\!\\rightarrow \\!\\! \\text{ZZ} \\!\\!\\rightarrow \\!\\! 4\\ell',
+    'shortName' : 'ggZZ2e2mu',
+    'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 2e2\\mu',
     'isSignal' : True,
-    'color' : EColor.kOrange+7,
+    'color' : 'aliceblue',
 }
-sampleInfo["GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 43.62 * 0.000294,
-    'n' : 493461,
+
+sampleInfo["GluGluToZZTo4mu_BackgroundOnly_13TeV_MCFM"] = {
+    'xsec' : 0.00158582,
+    'n' : 348800,
     'isData' : False,
-    'shortName' : 'ggHZZ4l',
-    'prettyName' : '\\text{gg}\\!\\! \\rightarrow \\!\\! \\text{H} \\!\\! \\rightarrow \\!\\! \\text{ZZ} \\!\\! \\rightarrow \\!\\! 4\\ell',
+    'shortName' : 'ggZZ4mu',
+    'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\mu',
     'isSignal' : True,
-    'color' : EColor.kRed,
+    'color' : 'lightblue',
 }
-sampleInfo["TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 424.5,
-    'n' : 25474122,
+
+sampleInfo["GluGluToZZTo4e_BackgroundOnly_13TeV_MCFM"] = {
+    'xsec' : 0.00158582,
+    'n' : 345600,
+    'isData' : False,
+    'shortName' : 'ggZZ4e',
+    'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4e',
+    'isSignal' : True,
+    'color' : 'lightsteelblue',
+}
+
+sampleInfo["TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
+    'xsec' : 670.3,
+    'n' : 4994250,
     'isData' : False,
     'shortName' : 'TTJets',
     'prettyName' : 't \\={t}\\text{+Jets}',
     'isSignal' : False,
-    'color' : EColor.kGreen,
+    'color' : 'limegreen',
 }
-sampleInfo["ZZTo4L_Tune4C_13TeV-powheg-pythia8_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 1.2,
-    'n' : 1958600,
+
+sampleInfo["WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8"] = {
+    'xsec' : 4.42965,
+    'n' : 1988000,
+    'isData' : False,
+    'shortName' : 'WZJets',
+    'prettyName' : '\\text{WZ+Jets}',
+    'isSignal' : False,
+    'color' : 'violet'
+}
+
+sampleInfo["ZZTo4L_13TeV_powheg_pythia8"] = {
+    'xsec' : 1.256,
+    'n' : 6745554,
     'isData' : False,
     'shortName' : 'ZZ4l',
     'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\ell',
-    'isSignal' : True,
-    'color' : EColor.kViolet,
-}
-sampleInfo["WZJetsTo3LNu_Tune4C_13TeV-madgraph-tauola_Spring14miniaod_PU20bx25"] = {
-    'xsec' : 1.634,
-    'n' : 240363,
-    'isData' : False,
-    'shortName' : 'WZJets',
-    'prettyName' : '\\text{WZ+Jets}',
     'isSignal' : False,
-    'color' : EColor.kCyan,
+    'color' : 'skyblue',
 }
 
-sampleInfo["DYJetsToLL_M-50_13TeV-madgraph-pythia8_PHYS14DR_PU20bx25"] = {
-    'xsec' : 6025.2,
-    'n' : 2829164,
-    'isData' : False,
-    'shortName' : 'DYJets',
-    'prettyName' : '\\text{DY+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kGreen+3,
-}
-sampleInfo["GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6_PHYS14DR_PU20bx25"] = {
-    'xsec' : 43.62 * 0.000294,
-    'n' : 204684,
-    'isData' : False,
-    'shortName' : 'ggHZZ4l',
-    'prettyName' : '\\text{gg}\\!\\! \\rightarrow \\!\\! \\text{H} \\!\\! \\rightarrow \\!\\! \\text{ZZ} \\!\\! \\rightarrow \\!\\! 4\\ell',
-    'isSignal' : True,
-    'color' : EColor.kRed,
-}
-sampleInfo["TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_PHYS14DR_PU20bx25"] = {
-    'xsec' : 424.5,
-    'n' : 25446993,
-    'isData' : False,
-    'shortName' : 'TTJets',
-    'prettyName' : 't \\={t}\\text{+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kGreen,
-}
-sampleInfo["ZZTo4L_Tune4C_13TeV-powheg-pythia8_PHYS14DR_PU20bx25"] = {
-    'xsec' : 1.2,
-    'n' : 1958600,
-    'isData' : False,
-    'shortName' : 'ZZ4l',
-    'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\ell',
-    'isSignal' : True,
-    'color' : EColor.kAzure-9,
-}
-sampleInfo["WZJetsTo3LNu_Tune4C_13TeV-madgraph-tauola_PHYS14DR_PU20bx25"] = {
-    'xsec' : 1.634,
-    'n' : 237484,
-    'isData' : False,
-    'shortName' : 'WZJets',
-    'prettyName' : '\\text{WZ+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kViolet,
-}
-
-sampleInfo["WZJetsTo3LNu_TuneZ2_8TeV-madgraph-tauola_Summer12DR53X_PUS10"] = {
-    'xsec' : 0.8674,
-    'n' : 1900000,
-    'isData' : False,
-    'shortName' : 'WZJets',
-    'prettyName' : '\\text{WZ+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kTeal-1,
-}
-sampleInfo["ZZTo4e_8TeV-powheg-pythia6_Summer12DR53X_PUS10"] = {
-    'xsec' : 0.07691,
-    'n' : 1500000,
-    'isData' : False,
-    'shortName' : 'ZZ4e',
-    'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\text{e}',
-    'isSignal' : True,
-    'color' : EColor.kMagenta-4,
-}
-sampleInfo["ZZTo4mu_8TeV-powheg-pythia6_Summer12DR53X_PUS10"] = {
-    'xsec' : 0.07691,
-    'n' : 1500000,
-    'isData' : False,
-    'shortName' : 'ZZ4mu',
-    'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\mu',
-    'isSignal' : True,
-    'color' : EColor.kMagenta-4,
-}
-sampleInfo["ZZTo2e2mu_8TeV-powheg-pythia6_Summer12DR53X_PUS10"] = {
-    'xsec' : 0.1767,
-    'n' : 1500000,
-    'isData' : False,
-    'shortName' : 'ZZ2e2mu',
-    'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 2e2\\mu',
-    'isSignal' : True,
-    'color' : EColor.kMagenta-4,
-}
-sampleInfo["DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12DR53X_PUS10"] = {
-    'xsec' : 2950.0,
-    'n' : 500000,
-    'isData' : False,
-    'shortName' : 'DYJets',
-    'prettyName' : '\\text{DY+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kBlue,
-}
-
-sampleInfo["HZZSync"] = {
-    'xsec' : 43.62 * 0.000294,
-    'n' : 10400,
-    'isData' : False,
-    'shortName' : 'HZZSync',
-    'prettyName' : '\\text{gg}\\!\\! \\rightarrow \\!\\! \\text{H} \\!\\! \\rightarrow \\!\\! \\text{ZZ} \\!\\! \\rightarrow \\!\\! 4\\ell',
-    'isSignal' : True,
-    'color' : EColor.kRed,
-}
-sampleInfo["TTBar8TeV"] = {
-    'xsec' : 53.2,
-    'n' : 30000000,
-    'isData' : False,
-    'shortName' : 'TTBar',
-    'prettyName' : 't \\={t}\\text{+Jets}',
-    'isSignal' : False,
-    'color' : EColor.kGreen,
-}
-sampleInfo["ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola_Summer12DR53X_PU10"] = {
-    'n' : 5000000,
-    'isData' : False,
-    'isSignal' : True,
-    'color' : EColor.kAzure-9,
-    'xsec' : 0.1296,
-    'shortName' : 'ZZ4l8TeV',
-    'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\ell',    
-}
