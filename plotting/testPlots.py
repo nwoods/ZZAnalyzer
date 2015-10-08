0835cc784e746c757f740a35b0b321151cc7c62c
Nate <nwoods@hep.wisc.edu>
2015-10-08 14:46:26 +0200

    New ntuples and output directory, don't draw test ratio plot since that works now.

--- plotting/testPlots.py
+++ plotting/testPlots.py
@@ -1,9 +1,10 @@
 from NtuplePlotter import NtuplePlotter
 from ZZHelpers import Z_MASS
 
-plotter = NtuplePlotter('zz', './plots/mcPlots25ns_anDraft', 
-                        {'mc':'/data/nawoods/ntuples/zzNtuples25ns_anDraft_0/results/*.root'}, 
-                        {'data':'/data/nawoods/ntuples/zzNtuples25ns_anDraft_0/results/data*.root'}, intLumi=1000)
+plotter = NtuplePlotter('zz', './plots/dataMC2015D_8oct2015', 
+                        {'mc':'/data/nawoods/ntuples/zzNtuples_mc_5oct2015_0/results/*.root'}, 
+                        {'data':'/data/nawoods/ntuples/zzNtuples_data_2015d_5oct2015_test_0/results/data*.root'}, 
+                        intLumi=180)
 
 for channel in ['eeee', 'zz', 'eemm', 'mmmm']:
 
@@ -42,20 +43,20 @@ plotter.fullPlot('mZ2_total', zPlotChannels, z2PlotVariables, zPlotSelections,
                  [30, 60., 120], 'mc', 'data', canvasX=1000, logy=False, xTitle="m_{Z_{2}}", 
                  outFile='mZ2.png', )
 
-ratioSample = plotter.ntuples['mc'].keys()[0]
-ratioPlot = plotter.Drawing("ratioPlot", plotter.style, 800, 1000)
-num = plotter.makeHist("mc", ratioSample, 'zz', 
-                       'MassFSR', "", [40, 0., 800.], -1., "", perUnitWidth=False)
-num.color = 'b'
-num.drawstyle = 'ep'
-ratioPlot.addObject(num, legendStyle="LPE")
-denom = plotter.makeHist("mc", ratioSample, 'zz', 
-                         'Mass', "", [40, 0., 800.], -1., "", perUnitWidth=False)
-denom.color = 'r'
-denom.drawstyle = 'ep'
-ratioPlot.addObject(denom, legendStyle="LPE")
-ratioPlot.addRatio(num, denom, 0.23)
-ratioPlot.draw(outFile="./plots/ratioTest.png", xTitle="m_{4\\ell}", 
-               xUnits="GeV", yTitle="Events", stackErr=False,
-               intLumi=-1., simOnly=True)
+# ratioSample = plotter.ntuples['mc'].keys()[0]
+# ratioPlot = plotter.Drawing("ratioPlot", plotter.style, 800, 1000)
+# num = plotter.makeHist("mc", ratioSample, 'zz', 
+#                        'MassFSR', "", [40, 0., 800.], -1., "", perUnitWidth=False)
+# num.color = 'b'
+# num.drawstyle = 'ep'
+# ratioPlot.addObject(num, legendStyle="LPE")
+# denom = plotter.makeHist("mc", ratioSample, 'zz', 
+#                          'Mass', "", [40, 0., 800.], -1., "", perUnitWidth=False)
+# denom.color = 'r'
+# denom.drawstyle = 'ep'
+# ratioPlot.addObject(denom, legendStyle="LPE")
+# ratioPlot.addRatio(num, denom, 0.23)
+# ratioPlot.draw(outFile="./plots/ratioTest.png", xTitle="m_{4\\ell}", 
+#                xUnits="GeV", yTitle="Events", stackErr=False,
+#                intLumi=-1., simOnly=True)
 
