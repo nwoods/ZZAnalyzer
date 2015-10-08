e62509f0f3f120c740caff638aeb700f96a5ff14
Nate <nwoods@hep.wisc.edu>
2015-10-08 14:38:27 +0200

    Add gg->ZZ->4tau sample, store sum of gen weights for samples.

--- ZZMetadata/ZZMetadata.py
+++ ZZMetadata/ZZMetadata.py
@@ -29,6 +29,7 @@ sampleInfo['data'] = {
 sampleInfo["DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
     'xsec' : 6104., 
     'n' : 28825132,
+    'sumW' : 452776427520.,
     'isData' : False,
     'shortName' : 'DYJets',
     'prettyName' : '\\text{DY+Jets}',
@@ -39,6 +40,7 @@ sampleInfo["DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
 sampleInfo["GluGluToZZTo2e2mu_BackgroundOnly_13TeV_MCFM"] = {
     'xsec' : 0.00319364,
     'n' : 649600,
+    'sumW' : 648800.,
     'isData' : False,
     'shortName' : 'ggZZ2e2mu',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 2e2\\mu',
@@ -49,6 +51,7 @@ sampleInfo["GluGluToZZTo2e2mu_BackgroundOnly_13TeV_MCFM"] = {
 sampleInfo["GluGluToZZTo2mu2tau_BackgroundOnly_13TeV_MCFM"] = {
     'xsec' : 0.00319364,
     'n' : 642000,
+    'sumW' : 650000.,
     'isData' : False,
     'shortName' : 'ggZZ2mu2tau',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 2\\mu2\\tau',
@@ -59,6 +62,7 @@ sampleInfo["GluGluToZZTo2mu2tau_BackgroundOnly_13TeV_MCFM"] = {
 sampleInfo["GluGluToZZTo2e2tau_BackgroundOnly_13TeV_MCFM"] = {
     'xsec' : 0.00319364,
     'n' : 647800,
+    'sumW' : 646200.,
     'isData' : False,
     'shortName' : 'ggZZ2e2tau',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 2e2\\tau',
@@ -69,6 +73,7 @@ sampleInfo["GluGluToZZTo2e2tau_BackgroundOnly_13TeV_MCFM"] = {
 sampleInfo["GluGluToZZTo4mu_BackgroundOnly_13TeV_MCFM"] = {
     'xsec' : 0.00158582,
     'n' : 338800,
+    'sumW' : 338800.,
     'isData' : False,
     'shortName' : 'ggZZ4mu',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\mu',
@@ -79,6 +84,7 @@ sampleInfo["GluGluToZZTo4mu_BackgroundOnly_13TeV_MCFM"] = {
 sampleInfo["GluGluToZZTo4e_BackgroundOnly_13TeV_MCFM"] = {
     'xsec' : 0.00158582,
     'n' : 348800,
+    'sumW' : 348800.,
     'isData' : False,
     'shortName' : 'ggZZ4e',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4e',
@@ -86,9 +92,21 @@ sampleInfo["GluGluToZZTo4e_BackgroundOnly_13TeV_MCFM"] = {
     'color' : 'lightsteelblue',
 }
 
+sampleInfo["GluGluToZZTo4tau_BackgroundOnly_13TeV_MCFM"] = {
+    'xsec' : 0.00158582,
+    'n' : 348800,
+    'sumW' : 646200.,
+    'isData' : False,
+    'shortName' : 'ggZZ4tau',
+    'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\tau',
+    'isSignal' : True,
+    'color' : 'darkgray',
+}
+
 sampleInfo["TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
     'xsec' : 670.3,
     'n' : 42730273,
+    'sumW' : 90441818112.,
     'isData' : False,
     'shortName' : 'TTJets',
     'prettyName' : 't \\={t}\\text{+Jets}',
@@ -99,6 +117,7 @@ sampleInfo["TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"] = {
 sampleInfo["WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8"] = {
     'xsec' : 4.42965,
     'n' : 1925000,
+    'sumW' : 1925000.,
     'isData' : False,
     'shortName' : 'WZJets',
     'prettyName' : '\\text{WZ+Jets}',
@@ -109,6 +128,7 @@ sampleInfo["WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8"] = {
 sampleInfo["ZZTo4L_13TeV_powheg_pythia8"] = {
     'xsec' : 1.256,
     'n' : 6652512,
+    'sumW' : 6652512.,
     'isData' : False,
     'shortName' : 'ZZ4l',
     'prettyName' : '\\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\ell',
@@ -119,6 +139,7 @@ sampleInfo["ZZTo4L_13TeV_powheg_pythia8"] = {
 sampleInfo["GluGluHToZZTo4L_M125_13TeV_powheg_JHUgen_pythia8"] = {
     'xsec' : 0.01212,
     'n' : 479600,
+    'sumW' : 479600.,
     'isData' : False,
     'shortName' : 'ggHZZ4l',
     'prettyName' : '\\text{gg}\\!\\!\\rightarrow \\!\\! \\text{H} \\!\\!\\rightarrow \\!\\! \\text{ZZ}\\!\\!\\rightarrow \\!\\! 4\\ell',
