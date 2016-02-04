97a135430250792523826e81366364dd9b06c5dc
Nate <nwoods@hep.wisc.edu>
2016-02-04 15:33:22 +0100

    First commit of cut to specify Z mass window.

---
+++ ZZUtils/cutTemplates/ZMassWindow.py
@@ -0,0 +1,24 @@
+from Cutter import Cutter
+from ZZHelpers import *
+
+
+class ZMassWindow(Cutter):
+    '''
+    Require candidate mass near Z peak
+    '''
+    def getCutTemplate(self, *args):
+        temp = super(ZMassWindow, self).getCutTemplate()
+
+        temp['4lMass']['cuts'] = {
+            'Mass%s#lower'%self.fsrVar : (80., ">="),
+            'Mass%s#upper'%self.fsrVar : (100., "<"),
+            }
+        
+        return temp
+    
+
+    def setupCutFlow(self):
+        flow = super(ZMassWindow, self).setupCutFlow()
+        flow['4lMass'] = ('4lMass', [])
+
+        return flow
