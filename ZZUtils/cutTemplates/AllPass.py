0dc856695de34c9c7d2a79e184c6e484559899ee
Nate <nwoods@hep.wisc.edu>
2016-03-10 10:54:43 +0100

    Allow other cuts to be stacked on top of AllPass.

--- ZZUtils/cutTemplates/AllPass.py
+++ ZZUtils/cutTemplates/AllPass.py
@@ -18,21 +18,11 @@ class AllPass(Cutter.Cutter):
         super(AllPass, self).__init__("AllPass")
 
 
-    def getCutTemplate(self,*args):
-        '''
-        Template for all cuts
-        '''
-        cutTemplate = {}
-
-        return cutTemplate
-
-
     def setupCutFlow(self):
         '''
         Dictionary with order and (one-indexed) numbers of particles to cut on
         '''
-        flow = OrderedDict()
-        flow['Total'] = ('true', [])
+        flow = super(AllPass, self).setupCutFlow()
         
         return flow
 
@@ -41,8 +31,7 @@ class AllPass(Cutter.Cutter):
         '''
         Define functions that don't fit the template nicely
         '''
-        temp = self.getCutTemplate()
-        others = {}
+        others = super(AllPass, self).setupOtherCuts()
 
         return others
 
